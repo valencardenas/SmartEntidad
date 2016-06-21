62f5a229fea373db72714c00ccd69b1539146e52
Dragonarka <Daniel9116@gmail.com>
2016-06-20 21:02:30 -0500

    se agrega clase estudiantes para manejo base de datos

---
+++ app/src/main/java/com/example/valentina/smartestudiantes/EstudiantesClass.java
@@ -0,0 +1,23 @@
+package com.example.valentina.smartestudiantes;
+
+/**
+ * Created by Valentina on 20-Jun-16.
+ */
+public class EstudiantesClass {
+
+    private String nombre, apellidos, id, correo, tel , cel, contrato , fecha;
+
+    public EstudiantesClass (){
+
+    }
+
+    public String getNombre(){return nombre;}
+    public String getApellidos(){return apellidos;}
+    public String getid(){return id;}
+    public String getCorreo(){return correo;}
+    public String getTel(){return tel;}
+    public String getCel(){return cel;}
+    //aca viene lo de los libros pero no se como es que se obtiene la info de ahi
+    public String getContrato(){return contrato;}
+    public String getFecha(){return fecha;}
+}
